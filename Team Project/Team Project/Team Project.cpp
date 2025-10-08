2f4ab121b9ac9bb3344de1d299508170c5ee569b
Gage <gageb534@gmail.com>
2025-10-08 13:49:36 -0500

    gdx

--- Team Project/Team Project/Team Project.cpp
+++ Team Project/Team Project/Team Project.cpp
@@ -48,6 +48,18 @@ double arrival()
 {
 	// accept a double like XX.XX
 	// and make sure its >=00.00 and <= 24.00
+
+	double arrivalTime = -1.00;
+	cout << "Enter the arrival time (formatted as XX.XX, 24 hour clock) :> ";
+	cin >> arrivalTime;
+
+	while (arrivalTime <= 0.00 && arrivalTime >= 24.00)
+	{
+		cout << "Enter the arrival time (formatted as XX.XX, 24 hour clock) :> ";
+		cin >> arrivalTime;
+	}
+
+	return arrivalTime;
 }
 
 double roundTrip()
@@ -57,7 +69,16 @@ double roundTrip()
 
 double carRentals()
 {
-	
+	string carsUsed = "a";
+
+	cout << "Did you use a rental car on the trip? (y/n) :> ";
+	cin >> carsUsed;
+
+	while (carsUsed != "Y" && carsUsed != "y" && carsUsed != "N" && carsUsed != "n")
+	{
+		cout << "Did you use a rental car on the trip? (y/n) :> ";
+		cin >> carsUsed;
+	}
 }
 
 double milesDriven()
@@ -67,7 +88,39 @@ double milesDriven()
 
 double parkingFees()
 {
+	int daysFeed = -1;
+	double cost = -1, total;
+	const double companyPrice = 6.00;
 
+	while (daysFeed < 0)
+	{
+		cout << "Enter the number of days there were parking fees for (0 for none) :> ";
+		cin >> daysFeed;
+	}
+
+	if (daysFeed == 0)
+	{
+		return 0.0;
+	}
+	else
+	{
+		while (cost <= 0)
+		{
+			cout << "Enter the total cost for the parking fees :> ";
+			cin >> cost;
+		}
+
+		total = cost - (daysFeed * companyPrice);
+
+		if (total < 0.00)
+		{
+			return 0.00;
+		}
+		else
+		{
+			return total;
+		}
+	}
 }
 
 double taxiFees()
@@ -77,7 +130,15 @@ double taxiFees()
 
 double conferenceFees()
 {
-	
+	double cost = -1;
+
+	while (cost < 0)
+	{
+		cout << "Enter the fees for conference / seminars :> ";
+		cin >> cost;
+	}
+
+	return cost;
 }
 
 double lodgingFees()
@@ -87,5 +148,63 @@ double lodgingFees()
 
 double mealFees(double arrivalTime, double departureTime)
 {
+	double totalCost = -1.00, runningTotal = 0.0;
+	const double aBreakfast = 7.00, aLunch = 12.00, aDinner = 19.00, bBreakfast = 8.00, bLunch = 13.00, bDinner = 19.00;
+	double aBreakfastCost = -1, aLunchCost = -1, aDinnerCost = -1, bBreakfastCost = -1, bLunchCost = -1, bDinnerCost = -1;
 
+	while (totalCost < 0)
+	{
+		if (arrivalTime < aBreakfast)
+		{
+			while (aBreakfastCost < 0)
+			{
+				cout << "Enter the cost of breakfast on the day of the arrival :> ";
+				cin >> aBreakfastCost;
+			}
+
+			if (aBreakfastCost > 9)
+			{ }
+			runningTotal = aBreakfastCost - 9
+		}
+		if (arrivalTime < aLunch)
+		{
+			while (aLunchCost < 0)
+			{
+				cout << "Enter the cost of lunch on the day of the arrival :> ";
+				cin >> aLunchCost;
+			}
+		}
+		if (arrivalTime < aDinner)
+		{
+			while (aDinnerCost < 0)
+			{
+				cout << "Enter the cost of dinner on the day of the arrival :> ";
+				cin >> aDinnerCost;
+			}
+		}
+		if (departureTime < bBreakfast)
+		{
+			while (bBreakfastCost < 0)
+			{
+				cout << "Enter the cost of breakfast on the day of departure :> ";
+				cin >> bBreakfastCost;
+			}
+		}
+		if (departureTime < bLunch)
+		{
+			while (bLunchCost < 0)
+			{
+				cout << "Enter the cost of lunch on the day of departure :> ";
+				cin >> bLunchCost;
+			}
+		}
+		if (departureTime < bDinner)
+		{
+			while (bDinnerCost < 0)
+			{
+				cout << "Enter the cost of dinner on the day of departure :> ";
+				cin >> bDinnerCost;
+			}
+		}
+	}
 }
